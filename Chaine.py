fbc86b34225a215d45e68fd1e3bdb40f18989bbe
Este <email@mail.mail>
2019-02-18 10:14:31 +0100

    creation du fichier Chaine

---
+++ Chaine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 09:52:47 2019
+
+@author: gabriel
+"""
+
+class Chaine:
+    
+    def __init__(self,bloc):
+        bloc_courant = bloc
+    
+    def chercher_transaction(self,date,bloc,id_hash):
